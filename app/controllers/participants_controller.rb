d9c97e991f9fd1a82523b127267e96da797e3729
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2017-08-02 20:01:47 +0900

    Add columns to Participant Model, Associtaion Paraticipant and Ticket model

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,15 +2,16 @@ class ParticipantsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @event = Event.find(params[:participant][:event_id])
-    current_user.participate!(@event)
-    redirect_to event_path(@event), notice: "参加しました！"
+    event = Event.find(params[:participant][:event_id])
+    ticket = Ticket.find(params[:participant][:ticket_id])
+    current_user.participate!(event, ticket)
+    redirect_to event_path(event), notice: "参加しました！"
   end
 
   def destroy
-    @event = Participant.find(params[:id]).event
-    current_user.cancel!(@event)
-    redirect_to event_path(@event), notice: "キャンセルしました！"
+    event = Participant.find(params[:id]).event
+    current_user.cancel!(event)
+    redirect_to event_path(event), notice: "キャンセルしました！"
   end
 
   def kick_out
