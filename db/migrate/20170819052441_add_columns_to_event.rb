5a23c81fb1b8511f9fb96ea987e2b87236b72fbb
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2017-08-19 15:16:03 +0900

    Add Columns to Event model

---
+++ db/migrate/20170819052441_add_columns_to_event.rb
@@ -0,0 +1,7 @@
+class AddColumnsToEvent < ActiveRecord::Migration[5.1]
+  def change
+    add_column :events, :content, :text
+    add_column :events, :start_time, :datetime
+    add_column :events, :end_time, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816111417) do
+ActiveRecord::Schema.define(version: 20170819052441) do
 
   create_table "event_owners", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20170816111417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "participant_public_flg", default: true, null: false
+    t.text "content"
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "participants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
