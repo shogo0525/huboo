3d4c28fbec699391e8a5c18b489f2c5bc8e00786
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2017-08-31 11:08:34 +0900

    Impl cancel event

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources :events do
     get 'copy', action: :new
+    post 'cancel', on: :member, action: :cancel
   end
   resources :tickets
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819052441) do
+ActiveRecord::Schema.define(version: 20170831015544) do
 
   create_table "event_owners", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170819052441) do
     t.text "content"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.datetime "canceled_at"
   end
 
   create_table "participants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
